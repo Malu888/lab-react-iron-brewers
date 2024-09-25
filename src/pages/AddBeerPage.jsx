c1daf2c09b5a8847c6427460354dc24453709d90
unknown <maludietrich.00@gmail.com>
2024-09-26 00:23:52 +0200

    done

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
@@ -10,7 +13,7 @@ function AddBeerPage() {
   const [brewersTips, setBrewersTips] = useState("");
   const [attenuationLevel, setAttenuationLevel] = useState(0);
   const [contributedBy, setContributedBy] = useState("");
-
+  const navigate = useNavigate();
   // Handler functions for the form inputs. You can leave these as they are.
   const handleName = (e) => setName(e.target.value);
   const handleTagline = (e) => setTagline(e.target.value);
@@ -27,14 +30,36 @@ function AddBeerPage() {
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const addNewBeer = {
+      name,
+      tagline,
+      description,
+      imageUrl,
+      firstBrewed,
+      brewersTips,
+      attenuationLevel: Number(attenuationLevel),
+      contributedBy
+    }
+
+    try {
+
+      await axios.post(`${import.meta.env.VITE_SERVER_URL}/beers/new`, addNewBeer)
+
+      navigate('/beers')
+    } catch (error) {
+      console.log(`Erro ao criar a cerveja`, error)
+    }
+  }
 
 
   // Structure and the content of the page showing the form for adding a new beer. You can leave this as it is.
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
@@ -122,7 +147,7 @@ function AddBeerPage() {
             value={contributedBy}
             onChange={handleContributedBy}
           />
-          <button className="btn btn-primary btn-round">Add Beer</button>
+          <button className="btn btn-primary btn-round" type="submit">Add Beer</button>
         </form>
       </div>
     </>
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,20 +1,35 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Search from "../components/Search";
-import beersJSON from "./../assets/beers.json";
+//import beersJSON from "./../assets/beers.json";
+import { useEffect } from "react";
+import axios from "axios";
 
 
+//<Route path="/beers/:beerId" element={<BeerDetailsPage />} />
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
-
+  //const [beers, setBeers] = useState(beersJSON); 
+  const [newBeers, setNewBeers] = useState(null)
 
 
   // TASKS:
   // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
+useEffect (() => {
+  axios.get(`${import.meta.env.VITE_SERVER_URL}/beers`)
+
+  .then((response) => {
+    console.log(response)
+    setNewBeers(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+}, [])
+
 
 
 
@@ -24,24 +39,24 @@ function AllBeersPage() {
       <Search />
 
       <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
-        {beers &&
-          beers.map((beer, i) => {
+        {newBeers &&
+          newBeers.map((eachBeer, i) => {
             return (
               <div key={i}>
-                <Link to={"/beers/" + beer._id}>
+                <Link to={"/beers/" + eachBeer._id}>
                   <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
                     <div className="card-body">
                       <img
-                        src={beer.image_url}
+                        src={eachBeer.image_url}
                         style={{ height: "6rem" }}
-                        alt={"image of" + beer.name}
+                        alt={"image of" + eachBeer.name}
                       />
-                      <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
+                      <h5 className="card-title text-truncate mt-2">{eachBeer.name}</h5>
                       <h6 className="card-subtitle mb-3 text-muted">
-                        <em>{beer.tagline}</em>
+                        <em>{eachBeer.tagline}</em>
                       </h6>
                       <p className="card-text">
-                        Created by: {beer.contributed_by}
+                        Created by: {eachBeer.contributed_by}
                       </p>
                     </div>
                   </div>
